c528e01db402ecd544bced9ea3079bed11d17732
jonoman55 <jonoman55@yahoo.com>
2022-03-02 11:51:00 -0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { Spinner } from './components/Spinner';
 import PostDetails from './components/PostDetails';
 import ProfileDetails from './components/ProfileDetails';
 
-function App() {
-    return (
-        <div className='container my-5'>
-            <Suspense fallback={<Spinner />}>
-                <ProfileDetails />
-                <PostDetails />
-            </Suspense>
-        </div>
-    );
-};
+const App = () => (
+    <div className='container my-5'>
+        <Suspense fallback={<Spinner />}>
+            <ProfileDetails />
+            <PostDetails />
+        </Suspense>
+    </div>
+);
 
 export default App;
